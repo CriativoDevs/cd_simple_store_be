e944fccc05a2fdfe8a7025adca103fad02b3a429
pablo <pablo@pablos-MBP-M2.lan>
2024-05-15 11:23:41 +0100

    Adding allowed host

--- api/api/wsgi.py
+++ api/api/wsgi.py
@@ -8,9 +8,23 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+# Path to your project directory
+path = "/home/criativodevs/cd_simple_store_be"
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
+
+# Activate your virtual environment
+activate_this = "/home/criativodevs/.virtualenvs/myenv/bin/activate_this.py"
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+# Import Django and set up the application
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
