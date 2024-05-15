2dc8098c50b6c65416d0834c00ad33e40c31e4db
pablo <pablo@pablos-MBP-M2.lan>
2024-05-15 12:48:19 +0100

    Adding allowed host

--- api/api/wsgi.py
+++ api/api/wsgi.py
@@ -19,11 +19,6 @@ if path not in sys.path:
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
 
-# Activate your virtual environment
-activate_this = "/home/criativodevs/.virtualenvs/myenv/bin/activate_this.py"
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
-
 # Import Django and set up the application
 from django.core.wsgi import get_wsgi_application
 
