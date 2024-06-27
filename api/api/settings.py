cf66373470c4baad7d6b447c80bfac0a021d02a6
pablo <pablo@pablos-MBP-M2.lan>
2024-06-27 18:40:56 +0100

    Updating requirements

--- api/api/settings.py
+++ api/api/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "http://criativo.pythonanywhere.com/"]
 
 # Domain for email activation
 HOST_URL = ["127.0.0.1:8000"]
