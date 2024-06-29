8d53f7e264bf89075d784863077618f3afb8fc27
pablo <pablo@pablos-MBP-M2.lan>
2024-06-29 16:57:10 +0100

    Changes

--- api/docesal_be/admin.py
+++ api/docesal_be/admin.py
@@ -5,4 +5,10 @@ from .models import Product, UserProfile, Purchase, LogEntry
 admin.site.register(Product)
 admin.site.register(UserProfile)
 admin.site.register(Purchase)
-admin.site.register(LogEntry)
+
+
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ("timestamp", "level", "module", "message")
+    list_filter = ("level", "timestamp")
+    search_fields = ("message", "module")
