f114767fa809a2ab2352dfc40d8e5b867ced02a3
pablo <pablo@pablos-MBP-M2.lan>
2024-07-03 17:47:31 +0100

    Changes

--- api/docesal_be/utils.py
+++ api/docesal_be/utils.py
@@ -110,9 +110,9 @@ def send_email_with_pdf(to_email, subject, body, pdf_buffer):
         msg.attach(MIMEText(body, "plain"))
 
         # Attach the PDF file
-        pdf_bytes = pdf_buffer.getvalue()
+        pdf_bytes = bytes(pdf_buffer.getvalue())
         logger.info(f"PDF buffer type: {type(pdf_bytes)}")  # Debug statement
-        print(f"PDF buffer type: {type(pdf_bytes)}")
+        logger.info(f"PDF buffer length: {len(pdf_bytes)}")
         attach = MIMEApplication(pdf_bytes, _subtype="pdf")
         attach.add_header("Content-Disposition", "attachment", filename="receipt.pdf")
         msg.attach(attach)
