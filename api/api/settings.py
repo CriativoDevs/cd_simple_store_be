fb8e06ebabc87b7b04d80028f51c90eb0136abec
pablo <pablo@pablos-MBP-M2.lan>
2024-07-03 16:25:57 +0100

    Changes

--- api/api/settings.py
+++ api/api/settings.py
@@ -16,7 +16,9 @@ import os
 import logging
 from dotenv import load_dotenv
 
-load_dotenv()
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(dotenv_path)
 logger = logging.getLogger(__name__)
 
 # for forcetext debug code
