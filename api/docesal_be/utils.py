675c3795de8adf6c7a22b91a0e7a1fa466d93319
pablo <pablo@pablos-MBP-M2.lan>
2024-07-03 18:11:47 +0100

    Changes

--- api/docesal_be/utils.py
+++ api/docesal_be/utils.py
@@ -88,12 +88,10 @@ def send_email_with_pdf(to_email, subject, body, pdf_buffer):
             server = smtplib.SMTP_SSL(
                 settings.EMAIL_HOST, settings.EMAIL_PORT, timeout=settings.EMAIL_TIMEOUT
             )
-            logger.info("SSL connection established", server)
         else:
             server = smtplib.SMTP(
                 settings.EMAIL_HOST, settings.EMAIL_PORT, timeout=settings.EMAIL_TIMEOUT
             )
-            logger.info("Else SSL connection established", server)
             server.ehlo()
             if settings.EMAIL_USE_TLS:
                 server.starttls()
@@ -109,15 +107,15 @@ def send_email_with_pdf(to_email, subject, body, pdf_buffer):
         msg["Subject"] = subject
         msg.attach(MIMEText(body, "plain"))
 
-        # Attach the PDF file
+        # Ensure pdf_buffer is read as bytes
         try:
-            pdf_bytes = bytes(pdf_buffer.getvalue())
+            pdf_bytes = pdf_buffer.getvalue()
+            logger.info(f"PDF buffer type: {type(pdf_bytes)}")
+            logger.info(f"PDF buffer length: {len(pdf_bytes)}")
         except Exception as e:
-            logger.error(f"Error converting PDF buffer to bytes: {e}")
+            logger.error("Error converting PDF buffer to bytes: %s", e)
             raise
 
-        logger.info(f"PDF buffer type: {type(pdf_bytes)}")  # Debug statement
-        logger.info(f"PDF buffer length: {len(pdf_bytes)}")
         attach = MIMEApplication(pdf_bytes, _subtype="pdf")
         attach.add_header("Content-Disposition", "attachment", filename="receipt.pdf")
         msg.attach(attach)
@@ -128,13 +126,14 @@ def send_email_with_pdf(to_email, subject, body, pdf_buffer):
         logger.info("Email sent successfully")
         server.quit()
     except smtplib.SMTPException as e:
-        logger.info(f"SMTP error occurred: {e}")
+        logger.error("SMTP error occurred: %s", e)
     except Exception as e:
-        logger.info(f"Error sending email: {e}")
+        logger.error("Error sending email: %s", e)
 
 
-logger.info(f"EMAIL_HOST: {settings.EMAIL_HOST}")
-logger.info(f"EMAIL_PORT: {settings.EMAIL_PORT}")
-logger.info(f"EMAIL_USE_SSL: {settings.EMAIL_USE_SSL}")
-logger.info(f"EMAIL_HOST_USER: {settings.EMAIL_HOST_USER}")
-logger.info(f"EMAIL_HOST_PASSWORD: {settings.EMAIL_HOST_PASSWORD}")
+# Ensure the settings are loaded and correct
+logger.info("EMAIL_HOST: %s", settings.EMAIL_HOST)
+logger.info("EMAIL_PORT: %d", settings.EMAIL_PORT)
+logger.info("EMAIL_USE_SSL: %s", settings.EMAIL_USE_SSL)
+logger.info("EMAIL_HOST_USER: %s", settings.EMAIL_HOST_USER)
+logger.info("EMAIL_HOST_PASSWORD: %s", settings.EMAIL_HOST_PASSWORD)
