a174dfa85d9c9b90cfe1bf88b5fb6ec14b7832b0
pablo <pablo@pablos-MBP-M2.lan>
2024-07-03 18:04:15 +0100

    Changes

--- api/docesal_be/utils.py
+++ api/docesal_be/utils.py
@@ -110,7 +110,12 @@ def send_email_with_pdf(to_email, subject, body, pdf_buffer):
         msg.attach(MIMEText(body, "plain"))
 
         # Attach the PDF file
-        pdf_bytes = bytes(pdf_buffer.getvalue())
+        try:
+            pdf_bytes = bytes(pdf_buffer.getvalue())
+        except Exception as e:
+            logger.error(f"Error converting PDF buffer to bytes: {e}")
+            raise
+
         logger.info(f"PDF buffer type: {type(pdf_bytes)}")  # Debug statement
         logger.info(f"PDF buffer length: {len(pdf_bytes)}")
         attach = MIMEApplication(pdf_bytes, _subtype="pdf")
