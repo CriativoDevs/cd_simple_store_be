6a3eb16097df75cc731e4cf8ed5de3e0d044d3ff
pablo <pablo@pablos-MBP-M2.lan>
2024-07-08 16:44:03 +0100

    Changes

--- api/docesal_be/views.py
+++ api/docesal_be/views.py
@@ -14,7 +14,12 @@ from django.contrib.auth.hashers import make_password
 from django.template.loader import render_to_string
 from django.core.mail import EmailMultiAlternatives, EmailMessage
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.utils.encoding import force_bytes, force_text, DjangoUnicodeDecodeError, force_str
+from django.utils.encoding import (
+    force_bytes,
+    force_text,
+    DjangoUnicodeDecodeError,
+    force_str,
+)
 from django.conf import settings
 from django.views.generic import View
 from django.db.models import Q
@@ -214,24 +219,24 @@ class RegisterUser(views.APIView):
                 logger.error(f"Error creating user: {message}")
                 return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
-            user = User.objects.create(
-                first_name=data["first_name"],
-                last_name=data["last_name"],
-                username=data["email"],
-                email=data["email"],
-                password=make_password(data["password"]),
-                is_active=False,
-            )
+            with transaction.atomic():
+                user = User.objects.create(
+                    first_name=data["first_name"],
+                    last_name=data["last_name"],
+                    username=data["email"],
+                    email=data["email"],
+                    password=make_password(data["password"]),
+                    is_active=False,
+                )
 
-            logger.info(f"User created: {user}")
+                logger.info(f"User created: {user}")
 
-            email_subject = "Activate your account"
-            uid = force_text(urlsafe_base64_encode(force_bytes(user.pk)))
-            token = generate_token.make_token(user)
+                email_subject = "Activate your account"
+                uid = force_str(urlsafe_base64_encode(force_bytes(user.pk)))
+                token = generate_token.make_token(user)
 
-            domain = settings.HOST_URL
+                domain = settings.HOST_URL
 
-            try:
                 message = render_to_string(
                     "activate.html",
                     {
@@ -241,31 +246,30 @@ class RegisterUser(views.APIView):
                         "token": token,
                     },
                 )
-            except Exception as e:
-                logger.error(f"Error rendering template: {str(e)}")
-                logger.error(traceback.format_exc())
-                raise e
-            logger.info(f"The message is: {message}, Email subject: {email_subject}")
+                logger.info(
+                    f"The message is: {message}, Email subject: {email_subject}"
+                )
 
-            text_message = f"""
-            Hi {user.first_name} {user.last_name},
+                # Create a plain text message for email clients that don't support HTML
+                text_message = f"""
+                Hi {user.first_name} {user.last_name},
 
-            Please click the link below to verify your account:
+                Please click the link below to verify your account:
 
-            http://{domain}{reverse('activate', kwargs={'uidb64': uid, 'token': token})}
-            """
+                http://{domain}{reverse('activate', kwargs={'uidb64': uid, 'token': token})}
+                """
 
-            email_message = EmailMultiAlternatives(
-                email_subject,
-                text_message,
-                settings.EMAIL_HOST_USER,
-                [data["email"]],
-            )
+                email_message = EmailMultiAlternatives(
+                    email_subject,
+                    text_message,
+                    settings.EMAIL_HOST_USER,
+                    [data["email"]],
+                )
+                email_message.attach_alternative(message, "text/html")
 
-            email_message.attach_alternative(message, "text/html")
-            logger.info(f"Email message: {email_message}")
-
-            EmailThread(email_message).start()
+                # Send email in a thread
+                EmailThread(email_message).start()
+                logger.info(f"Email sent: {email_message}")
 
             activation_message = {"detail": "Check your email for activation link"}
             logger.info(f"Activation message: {activation_message}")
@@ -275,7 +279,6 @@ class RegisterUser(views.APIView):
         except Exception as e:
             message = {"detail": f"{str(e)}"}
             logger.error(f"Error creating user: {str(e)}")
-            logger.error(traceback.format_exc())
             return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
