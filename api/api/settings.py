5fa9dfc19d8da0e9e3cc11a94f8b986b644603f6
pablo <pablo@pablos-MBP-M2.lan>
2024-07-16 11:38:59 +0100

    Setting page size

--- api/api/settings.py
+++ api/api/settings.py
@@ -109,7 +109,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 20,
+    "PAGE_SIZE": 15,
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
